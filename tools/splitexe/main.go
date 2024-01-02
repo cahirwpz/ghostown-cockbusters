0f86e1525fbfd341a6846f677db0c936e496915d
Krystian Bacławski <krystian.baclawski@gmail.com>
2024-01-02 21:18:10 +0100

    Split into multiple files, relocs need to be fixed yet.

--- tools/splitexe/main.go
+++ tools/splitexe/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
-	"ghostown.pl/hunk"
+	"fmt"
 	"os"
+
+	"ghostown.pl/hunk"
 )
 
 var printHelp bool
@@ -11,6 +13,17 @@ var printHelp bool
 func init() {
 	flag.BoolVar(&printHelp, "help", false,
 		"print help message and exit")
+}
+
+func writeExe(baseName string, num int, hunks []hunk.Hunk) {
+	println(hunks[0].String())
+
+	hunks = append(hunks, &hunk.HunkEnd{})
+	exeName := fmt.Sprintf("%s.%d", baseName, num)
+
+	if err := hunk.WriteFile(exeName, hunks); err != nil {
+		panic("failed to write Amiga Hunk file")
+	}
 }
 
 func main() {
@@ -26,7 +39,44 @@ func main() {
 		panic("failed to read Amiga Hunk file")
 	}
 
-	if err := hunk.WriteFile(flag.Arg(0)+".out", hunks); err != nil {
-		panic("failed to write Amiga Hunk file")
+	if hunks[0].Type() != hunk.HUNK_HEADER {
+		panic("Not an executable file")
 	}
+
+	var exeHunks []hunk.Hunk
+	var exeHeader *hunk.HunkHeader
+
+	header := hunks[0].(*hunk.HunkHeader)
+	println(header.String())
+	hunkNum := 0
+	exeNum := 0
+
+	for _, h := range hunks[1:] {
+		ht := h.Type()
+		if ht == hunk.HUNK_CODE {
+			if exeNum > 0 {
+				writeExe(flag.Arg(0), exeNum, exeHunks)
+			}
+			exeHeader = &hunk.HunkHeader{}
+			exeHunks = []hunk.Hunk{exeHeader}
+			exeNum += 1
+		}
+
+		if ht == hunk.HUNK_CODE || ht == hunk.HUNK_DATA ||
+			ht == hunk.HUNK_BSS || ht == hunk.HUNK_RELOC32 {
+
+			exeHunks = append(exeHunks, h)
+
+			fmt.Printf("%d: %s\n", exeNum, h.Type().String())
+
+			if ht != hunk.HUNK_RELOC32 {
+				exeHeader.Specifiers = append(exeHeader.Specifiers, header.Specifiers[hunkNum])
+				exeHeader.Hunks += 1
+				exeHeader.Last = exeHeader.Hunks - 1
+				hunkNum += 1
+			}
+		}
+	}
+
+	writeExe(flag.Arg(0), exeNum, exeHunks)
 }
