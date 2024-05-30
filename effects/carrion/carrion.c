3d0cf13eb5293266a1c6d4771e0e96c03989587e
Krystian Bacławski <krystian.baclawski@gmail.com>
2024-05-30 11:26:44 +0200

    Second image from Carrion.

--- effects/carrion/carrion.c
+++ effects/carrion/carrion.c
@@ -3,27 +3,27 @@
 #include "pixmap.h"
 #include "gfx.h"
 
-#include "data/carrion-conan-pal.c"
-#include "data/carrion-conan-data.c"
+#include "data/carrion-metro-pal.c"
+#include "data/carrion-metro-data.c"
 
 static CopListT *cp;
 
 static CopListT *MakeCopperList(void) {
-  CopListT *cp = NewCopList(100 + conan_height * (conan_cols_width + 1));
+  CopListT *cp = NewCopList(100 + carrion_height * (carrion_cols_width + 1));
 
-  CopSetupBitplanes(cp, &conan, conan_depth);
+  CopSetupBitplanes(cp, &carrion, carrion_depth);
 
   {
-    u_short *data = conan_cols_pixels;
+    u_short *data = carrion_cols_pixels;
     short i, j;
 
-    for (i = 0; i < conan_height; i++) {
-      for (j = 0; j < conan_cols_width; j++) {
+    for (i = 0; i < carrion_height; i++) {
+      for (j = 0; j < carrion_cols_width; j++) {
         CopMove16(cp, color[j], *data++);
       }
 
       /* Start exchanging palette colors at the end of line. */
-      CopWait(cp, Y(i-1), X(conan_width + 16));
+      CopWait(cp, Y(i-1), X(carrion_width + 16));
     }
   }
 
@@ -31,7 +31,8 @@ static CopListT *MakeCopperList(void) {
 }
 
 static void Init(void) {
-  SetupPlayfield(MODE_LORES, conan_depth, X(0), Y(0), conan_width, conan_height);
+  SetupPlayfield(MODE_LORES, carrion_depth,
+                 X(0), Y(0), carrion_width, carrion_height);
 
   cp = MakeCopperList();
   CopListActivate(cp);
