c174ec11adf2f9b4bc5da3b5b98da3ce2a104d71
Krystian Bacławski <krystian.baclawski@gmail.com>
2024-06-29 15:41:21 +0200

    Generate circular double helix.

--- effects/dna3d/data/dna_gen.py
+++ effects/dna3d/data/dna_gen.py
@@ -1,29 +1,56 @@
 #!/usr/bin/python3
 
-import math
+import argparse
 import sys
 import os.path
 from contextlib import redirect_stdout
+from math import sin, cos, pi
 
 
 def gen_double_helix(radius=1.0, pitch=0.5, turns=2, points_per_turn=100):
     points = int(points_per_turn * turns)
 
-    start = -math.pi * turns
-    end = math.pi * turns
+    start = -pi * turns
+    end = pi * turns
     vertices = []
 
     for i in range(points):
         theta = (end - start) * i / points + start
 
-        x = radius * math.cos(theta)
-        y = pitch * theta / (2.0 * math.pi)
-        z = radius * math.sin(theta)
+        x = radius * cos(theta)
+        y = pitch * theta / (2.0 * pi)
+        z = radius * sin(theta)
         vertices.append((x, y, z))
 
-        x = radius * math.cos(theta + math.pi)
-        y = pitch * theta / (2.0 * math.pi)
-        z = radius * math.sin(theta + math.pi)
+        x = radius * cos(theta + pi)
+        y = pitch * theta / (2.0 * pi)
+        z = radius * sin(theta + pi)
+        vertices.append((x, y, z))
+
+    return vertices
+
+
+def gen_circular_double_helix(radius=2.0, helix_radius=0.5,
+                              turns=2, points_per_turn=100):
+    points = int(points_per_turn * turns)
+
+    start = 0
+    end = 2 * pi
+    vertices = []
+
+    for i in range(points):
+        theta = (end - start) * i / points + start
+        phi = turns * (end - start) * i / points + start
+
+        x = (radius + helix_radius * cos(phi)) * cos(theta)
+        y = (radius + helix_radius * sin(phi)) * sin(theta)
+        z = helix_radius * sin(phi)
+        vertices.append((x, y, z))
+
+        phi += pi
+        x = (radius + helix_radius * cos(phi)) * cos(theta)
+        y = (radius + helix_radius * sin(phi)) * sin(theta)
+        z = helix_radius * sin(phi)
         vertices.append((x, y, z))
 
     return vertices
@@ -52,14 +79,25 @@ def print_mtl(name):
 
 
 if __name__ == "__main__":
-    path = sys.argv[1]
-    name = os.path.basename(path)
+    parser = argparse.ArgumentParser(
+        description="Generate Wavefront OBJ/MTL file with double helix.")
+    parser.add_argument("--circular", action="store_true",
+                        help="Generate circular double helix.")
+    parser.add_argument("path", metavar="PATH", type=str,
+                        help="Prefix of the target file.")
+    args = parser.parse_args()
 
-    dna = gen_double_helix(
-            radius=1.5, pitch=10.0, turns=1.0, points_per_turn=16)
+    name = os.path.basename(args.path)
 
-    with redirect_stdout(open(f"{path}.obj", "w")):
+    if args.circular:
+        dna = gen_circular_double_helix(
+                radius=2.5, helix_radius=0.5, turns=4.0, points_per_turn=10)
+    else:
+        dna = gen_double_helix(
+                radius=1.5, pitch=10.0, turns=1.0, points_per_turn=16)
+
+    with redirect_stdout(open(f"{args.path}.obj", "w")):
         print_obj(name, dna)
 
-    with redirect_stdout(open(f"{path}.mtl", "w")):
+    with redirect_stdout(open(f"{args.path}.mtl", "w")):
         print_mtl(name)
