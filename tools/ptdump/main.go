159037ce5b8a4ab74887273f3413e57812893deb
Krystian Bacławski <krystian.baclawski@gmail.com>
2024-08-16 10:47:51 +0200

    ptdump: add option to dump timing for each row position

--- tools/ptdump/main.go
+++ tools/ptdump/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
-	"ghostown.pl/misc"
 	"log"
 	"os"
 	"text/template"
@@ -35,23 +36,36 @@ func (cd ChanData) String() string {
 		cd.EffectParams)
 }
 
-func dumpModule(m Module, baseName string) {
+func dumpModule(m Module) string {
 	t, err := template.New("export").Parse(moduleReportTemplate)
 	if err != nil {
 		log.Fatal(err)
 	}
-	file, err := os.Create(baseName + "_report.txt")
+	var b bytes.Buffer
+	err = t.Execute(&b, m)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = t.Execute(file, m)
-	file.Close()
+	return b.String()
+}
+
+var (
+	printHelp bool
+	timing    bool
+)
+
+func init() {
+	flag.BoolVar(&printHelp, "help", false,
+		"print help message and exit")
+	flag.BoolVar(&timing, "timing", false,
+		"print timing for each row in the song")
 }
 
 func main() {
 	flag.Parse()
 
-	if len(flag.Args()) == 0 {
+	if len(flag.Args()) == 0 || printHelp {
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
@@ -60,7 +74,40 @@ func main() {
 		log.Fatal(err)
 	}
 
-	baseName := misc.PathWithoutExt(flag.Arg(0))
+	writer := bufio.NewWriter(os.Stdout)
+	mod := ReadModule(file)
 
-	dumpModule(ReadModule(file), baseName)
+	if timing {
+		framesPerRow := 6
+		time := 0.0
+		for _, pat := range mod.Song {
+			for i := 0; i < 64; i++ {
+				row := mod.Patterns[pat][i]
+				ts := fmt.Sprintf("%.2f", time)
+				fmt.Printf("%7ss | %02x/%02x %s\n", ts, pat, i, row)
+				for _, ch := range row {
+					if ch.Effect == 0xf {
+						if ch.EffectParams < 0x20 {
+							// F-speed
+							framesPerRow = int(ch.EffectParams)
+						} else {
+							// F-speed
+							if ch.EffectParams != 0x7D {
+								log.Fatal("only default CIA tempo is supported")
+							}
+						}
+					}
+				}
+				time += float64(framesPerRow) / 50.0
+			}
+		}
+	} else {
+		_, err = writer.WriteString(dumpModule(mod))
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+
+	writer.Flush()
 }
