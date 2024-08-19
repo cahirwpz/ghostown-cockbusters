05cf0bb1d73708db34181d67ebdb2028e046de04
Krystian Bacławski <krystian.baclawski@gmail.com>
2024-08-19 20:03:28 +0200

    Clean up Makefile and add logo effect (not attached yet)

---
+++ demo/logo/logo.c
@@ -0,0 +1,30 @@
+#include <effect.h>
+#include <copper.h>
+#include <blitter.h>
+#include <gfx.h>
+
+#include "data/logo.c"
+
+static CopListT *cp;
+
+static void Init(void) {
+  SetupPlayfield(MODE_LORES, logo_depth, X(0), Y(0), logo_width, logo_height);
+  LoadColors(logo_colors, 0);
+
+  cp = NewCopList(40);
+  CopSetupBitplanes(cp, &logo, logo_depth);
+  CopListFinish(cp);
+  CopListActivate(cp);
+
+  EnableDMA(DMAF_RASTER);
+}
+
+static void Kill(void) {
+  CopperStop();
+  DeleteCopList(cp);
+}
+
+static void Render(void) {
+}
+
+EFFECT(Logo, NULL, NULL, Init, Kill, Render, NULL);
