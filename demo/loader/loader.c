2ea5efec207fd20be496c18315c740f396c0b1f9
Czarek Stajszczyk <czarek.stajszczyk@protonmail.com>
2024-08-21 22:00:41 +0200

    Floating spaceship

--- demo/loader/loader.c
+++ demo/loader/loader.c
@@ -3,13 +3,14 @@
 #include <blitter.h>
 #include <gfx.h>
 #include <line.h>
+#include <sprite.h>
 
 #include "data/loader.c"
 #include "data/ufo.c"
 
 static __code BitmapT *screen;
 static __code CopListT *cp;
-// static short ufo_x = 16;
+static __code CopInsPairT *sprptr;
 
 #define X1 99
 #define Y1 230
@@ -30,21 +31,26 @@ static void Init(void) {
   CpuLine(X1 - 2, Y1 - 1, X1 - 2, Y2 + 1);
   CpuLine(X2 + 2, Y1 - 1, X2 + 2, Y2 + 1);
 
+  sprptr = CopSetupSprites(cp);
+  CopInsSetSprite(&sprptr[0], &ufo[0]);
+  CopInsSetSprite(&sprptr[1], &ufo[1]);
+  CopInsSetSprite(&sprptr[2], &ufo[2]);
+
   SetupPlayfield(MODE_LORES, DEPTH, X(0), Y(0), WIDTH, HEIGHT);
   LoadColors(loader_colors, 0);
+  LoadColors(ufo_colors, 16);
+  LoadColors(ufo_colors, 20);
 
   EnableDMA(DMAF_BLITTER);
   BitmapCopy(screen, 0, 0, &loader);
   WaitBlitter();
-  // BitmapCopyArea(screen, 16, 16, &ufo, &((Area2D){0, 0, 44, 15}));
-  // WaitBlitter();
   DisableDMA(DMAF_BLITTER);
 
   CopSetupBitplanes(cp, screen, DEPTH);
   CopListFinish(cp);
   CopListActivate(cp);
 
-  EnableDMA(DMAF_RASTER);
+  EnableDMA(DMAF_RASTER | DMAF_SPRITE);
 }
 
 static void Kill(void) {
@@ -55,7 +61,25 @@ static void Kill(void) {
 }
 
 static void MoveUfo(void) {
-  Log("Move ufo\n");
+  static short counter = 0;
+  static short ufo_y = 16;
+  static short dir = 1;
+
+  if (ufo_y <= 16) {
+    dir = 1;
+  } else if (ufo_y >= 22) {
+    dir = -1;
+  }
+
+  ++counter;
+  if (counter % 7 == 0) {
+    ufo_y += dir;
+  }
+
+  SpriteUpdatePos(&ufo[0], X(16), Y(ufo_y));
+  SpriteUpdatePos(&ufo[1], X(32), Y(ufo_y));
+  SpriteUpdatePos(&ufo[2], X(48), Y(ufo_y));
+
 }
 
 static void Render(void) {
