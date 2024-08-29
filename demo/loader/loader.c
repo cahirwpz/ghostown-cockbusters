5aa3864cbac2233a2468d299145b6811c898efab
Czarek Stajszczyk <czarek.stajszczyk@protonmail.com>
2024-08-29 14:21:23 +0200

    Loader: update background, No AGA, chickens!

--- demo/loader/loader.c
+++ demo/loader/loader.c
@@ -5,12 +5,14 @@
 #include <line.h>
 #include <sprite.h>
 #include <ptplayer.h>
+#include <color.h>
 
 #define _SYSTEM
 #include <system/cia.h>
 
 #include "data/loader.c"
 #include "data/ufo.c"
+#include "data/noaga.c"
 
 extern u_char LoaderModule[];
 extern u_char LoaderSamples[];
@@ -27,6 +29,8 @@ static __code CopInsPairT *sprptr;
 #define WIDTH 320
 #define HEIGHT 256
 #define DEPTH 3
+
+static bool show_noaga = false;
 
 static void Init(void) {
   PtInstallCIA();
@@ -47,10 +51,25 @@ static void Init(void) {
   CopInsSetSprite(&sprptr[1], &ufo[1]);
   CopInsSetSprite(&sprptr[2], &ufo[2]);
 
+  CopInsSetSprite(&sprptr[4], &noaga[0]);
+  CopInsSetSprite(&sprptr[5], &noaga[1]);
+  CopInsSetSprite(&sprptr[6], &noaga[2]);
+  CopInsSetSprite(&sprptr[7], &noaga[3]);
+
+  #define NOAGA_X 144
+  #define NOAGA_Y 12
+
+  SpriteUpdatePos(&noaga[0], X(NOAGA_X),    Y(NOAGA_Y));
+  SpriteUpdatePos(&noaga[1], X(NOAGA_X+16), Y(NOAGA_Y));
+  SpriteUpdatePos(&noaga[2], X(NOAGA_X+32), Y(NOAGA_Y));
+  SpriteUpdatePos(&noaga[3], X(NOAGA_X+48), Y(NOAGA_Y));
+
   SetupPlayfield(MODE_LORES, DEPTH, X(0), Y(0), WIDTH, HEIGHT);
   LoadColors(loader_colors, 0);
   LoadColors(ufo_colors, 16);
   LoadColors(ufo_colors, 20);
+  ITER(i, 24, 31, SetColor(i, 0xf90));
+
 
   EnableDMA(DMAF_BLITTER);
   BitmapCopy(screen, 0, 0, &loader);
@@ -80,6 +99,8 @@ static void MoveUfo(void) {
   static short counter = 0;
   static short ufo_y = 16;
   static short dir = 1;
+  static short s = 0;
+  short i = 0;
 
   if (ufo_y <= 16) {
     dir = 1;
@@ -96,11 +117,22 @@ static void MoveUfo(void) {
   SpriteUpdatePos(&ufo[1], X(32), Y(ufo_y));
   SpriteUpdatePos(&ufo[2], X(48), Y(ufo_y));
 
+  if (show_noaga && s < 16) {
+    for (i = 0; i < 4; ++i) {
+      SetColor(24+i, ColorTransition(0xf90, noaga_colors[i], s));
+      SetColor(28+i, ColorTransition(0xf90, noaga_colors[i], s));
+    }
+    ++s;
+  }
+
 }
 
 static void Render(void) {
   static __code short x = 0;
   short newX = frameCount >> 3;
+  if (newX > 24) {
+    show_noaga = true;
+  }
   if (newX > 121)
     newX = 122;
   for (; x < newX; x++) {
