4565cf5a3dcb1483a5d312409d20a32225008152
Krystian Bacławski <krystian.baclawski@gmail.com>
2025-02-16 10:08:05 +0100

    Minor debugging improvements.

--- lib/libgfx/CopperStop.c
+++ lib/libgfx/CopperStop.c
@@ -5,10 +5,10 @@ static __data_chip __aligned(2) u_int NullCopperList = 0xfffffffe;
 
 void CopperStop(void) {
   /* Load empty list and instruct copper to jump to them immediately. */
-  custom->cop1lc = (u_int)&NullCopperList;
   custom->cop2lc = (u_int)&NullCopperList;
-  (void)custom->copjmp1;
-  (void)custom->copjmp2;
+  custom->copjmp2 = 0;
+  custom->cop1lc = (u_int)&NullCopperList;
+  custom->copjmp1 = 0;
   /* Disable copper DMA and all systems that rely on frame-by-frame refresh
    * by Copper. */
   DisableDMA(DMAF_COPPER | DMAF_RASTER | DMAF_SPRITE);
