f22dae5987ed7ff57ef112d75ae6d3525420e407
Krystian Bacławski <krystian.baclawski@gmail.com>
2025-02-23 13:27:15 +0100

    Full support for CRNG & DRNG ILBM chunks.

--- demo/logo/logo.c
+++ demo/logo/logo.c
@@ -14,92 +14,43 @@ static __code CopListT *cp;
 typedef struct ColorCycling {
   short rate;
   short step;
-  short len;
-  short indices[16];
-  short colors[16];
+  short ncolors;
+  short *cells;
+  u_short *colors;
 } ColorCyclingT;
 
-static ColorCyclingT logo_cycling[] = {
-  {
-    .rate = 5632,
-    .step = 0,
-    .len = 8,
-    .indices = {26, 5, 7, 27, 27, 7, 5, 26},
-    .colors = {}
-  }, {
-    .rate = 5632,
-    .step = 0,
-    .len = 4,
-    .indices = {23, 24, 25, 23},
-    .colors = {}
-  }, {
-    .rate = 4608,
-    .step = 0,
-    .len = 8,
-    .indices = {10, 10, 29, 28, 10, 29, 10, 10},
-    .colors = {}
-  }, {
-    .rate = 3072,
-    .step = 0,
-    .len = 11,
-    .indices = {18, 22, 30, 31, 31, 31, 30, 22, 18, 18, 18},
-    .colors = {}
-  }
-};
+#include "data/logo-cycling.c"
 
 /* I genuinely hate this format. Reverse engineering it is PITA! */
-static void ColorCyclingInit(ColorCyclingT *rots, short len, u_short *colors) {
-  short i, j;
-
-  for (j = 0; j < len; j++) {
-    ColorCyclingT *rot = &rots[j];
-    rot->step = 0;
-    rot->rate = div16(1 << 24, rot->rate);
- 
-    for (i = 0; i < rot->len; i++)
-      rot->colors[i] = colors[rot->indices[i]];
-  }
-}
-
-#define FRAME ((1 << 24) / (1 << 14))
+#define PAL_FRAME ((1 << 16) / 50)
 
 static void ColorCyclingStep(ColorCyclingT *rots, short len) {
-  // From https://wiki.amigaos.net/wiki/ILBM_IFF_Interleaved_Bitmap#CRNG
-  // "The field rate determines the speed at which the colors will step when
-  // color cycling is on. The units are such that a rate of 60 steps per
-  // second is represented as 2^14 = 16384. Slower rates can be obtained
-  // by linear scaling: for 30 steps/second, rate = 8192; for 1 step/second,
-  // rate = 16384/60 ~273."
-  short j;
+  do {
+    ColorCyclingT *rot = rots++;
 
-  for (j = 0; j < len; j++) {
-    ColorCyclingT *rot = &rots[j];
-    short i;
-    u_short tmp;
+    rot->step += PAL_FRAME;
 
-    rot->step += FRAME;
-    if (rot->step < FRAME * 2)
-      continue;
-    rot->step -= FRAME * 2;
+    if (rot->step >= rot->rate) {
+      short *cells = rot->cells;
+      short reg;
 
-    tmp = rot->colors[0];
-    for (i = 0; i < rot->len - 1; i++) {
-      rot->colors[i] = rot->colors[i + 1];
+      rot->step -= rot->rate;
+
+      while ((reg = *cells++) >= 0) {
+        short idx = *cells + 1; 
+        if (idx == rot->ncolors)
+          idx = 0;
+        *cells++ = idx;
+
+        SetColor(reg, rot->colors[idx]);
+      }
     }
-    rot->colors[i] = tmp;
-
-    for (i = rot->len - 1; i >= 0; i--) {
-      SetColor(rot->indices[i], rot->colors[i]);
-    }
-
-  }
+  } while (--len > 0);
 }
 
 static void Init(void) {
   SetupPlayfield(MODE_LORES, logo_depth, X(0), Y(0), logo_width, logo_height);
   LoadColors(logo_colors, 0);
-
-  ColorCyclingInit(logo_cycling, nitems(logo_cycling), logo_colors);
 
   cp = NewCopList(40);
   CopSetupBitplanes(cp, &logo, logo_depth);
